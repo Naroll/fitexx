7f9f8d3d399f7f655f575f991a6d6112e72002ff
Naroll <firsovevgeny34@gmail.com>
2017-12-12 01:47:37 +0300

    Create recall form

--- app/controllers/recalls_controller.rb
+++ app/controllers/recalls_controller.rb
@@ -1,11 +1,15 @@
 class RecallsController < ApplicationController
 	
 	def new
-
 	end
 
 	def create
-
+		@recall = Recall.new(recall_params)
 	end
-	
+
+	private
+
+	def recall_params
+		params.require(:recall).permit(:name, :phone)
+	end
 end
