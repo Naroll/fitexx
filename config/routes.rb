6753ab6644ab7638cb25b4c89cf32ac47a211c82
Naroll <firsovevgeny34@gmail.com>
2017-12-12 09:32:48 +0300

    Backcalls routers.rb off

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
-  resources :backcalls
+  # resources :backcalls
 
   resource :recalls, only: [:new, :create]
 
   get '/recalls' => 'recalls#new'
+
+  root to: "recalls#new"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
